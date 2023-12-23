a0d5c0e81cabf9d0faf2e8839cf3832142bc4b28
Lance Yoder <lyoder@princeton.edu>
2023-12-23 17:20:12 -0500

    added 'see full recipe'

--- recipes.py
+++ recipes.py
@@ -1,11 +1,15 @@
 import random
 from fuzzywuzzy import process
+from enum import Enum
 
 RECIPE_FILE = r'data.txt'
 
+INTENSITIES = ['Fifteen minutes', 'Thirty minutes', 'Hour', 'Hour plus']
+
 class recipe:
-    def __init__(self, name, rating=0, link=None, recipeText=None, ingredients=None):
+    def __init__(self, name, intensity=None, rating=0, link=None, recipeText=None, ingredients=None):
         self._name = name
+        self._intensity = intensity
         self._rating = float(rating)
         self._ingredients = ingredients
         self._link = link
@@ -14,9 +18,10 @@ class recipe:
     def __str__(self):
         retStr = ""
         retStr += "Recipe: " + self._name + '\n'
+        retStr += "Intensity: " + self._intensity + '\n'
         retStr += "Rating: " + str(self._rating) + '\n'
         if self._ingredients:
-            retStr += "Ingredients: " + ", ".join(self._ingredients) + '\n'
+            retStr += "Main Ingredients: " + ", ".join(self._ingredients) + '\n'
         if self._link:
             retStr += "Link: " + self._link + '\n'
         if self._recipeText:
@@ -24,6 +29,10 @@ class recipe:
             retStr += self._recipeText + '\n'
 
         return retStr
+
+    def printMini(self):
+        print("Recipe Name: " + self._name + ", Intensity: " + self._intensity + ", Rating: " + str(self._rating))
+
 
 allIngredients = set()
 
@@ -34,8 +43,8 @@ def makeLists():
         lines = file.readlines()
         for line in lines:
             bits = line.split(',')
-            ingredients = [b.strip() for b in bits[2:]]
-            recipes.append(recipe(bits[0].strip(), bits[1].strip(), ingredients=ingredients))
+            ingredients = [b.strip() for b in bits[5:]]
+            recipes.append(recipe(bits[0].strip(), bits[1].strip(), bits[2].strip(), bits[3].strip(), bits[4].strip(), ingredients=ingredients))
             for b in ingredients:
                 allIngredients.add(b)
 
@@ -56,7 +65,7 @@ def suggestRecipe():
         print("This ingredient doesn't exist. Did you mean: ")
         for m in matches:
             print(m[0])
-        inputTag = input("Enter an ingredient:")
+        inputTag = input("Enter an ingredient: ")
         matches = process.extract(inputTag, allIngredients, limit=3)
         print()
     
@@ -65,26 +74,42 @@ def suggestRecipe():
         if matches[0][0] in rec._ingredients:
             randRec.append(rec)
 
-    print("Do you want all recipes (enter 1), one random suggestion (enter 2), or top recipes (enter 3)?")
+    print("Do you want all recipes (1), one random suggestion (2), or top recipes (3)?")
     allRec = input("Enter a number: ")
     print()
 
     if (allRec == '1'):
-        print(inputTag + " recipes:")
+        print(inputTag + " recipes: ")
         for rec in randRec:
-            print(rec._name + ", rating: " + str(rec._rating))
-
+            rec.printMini()
     elif (allRec == '2'):
         random.shuffle(randRec)
-        print("Suggestion:")
-        print(randRec[0]._name)
-        print("Rating: " + str(randRec[0]._rating))
+        print("Suggestion: ")
+        randRec[0].printMini()
+        print()
+        print("Would you like to see the full recipe?")
+        seeFull = input("Yes? ")
+        print()
+        if seeFull.lower() == 'yes':
+            print(randRec[0])
     elif (allRec == '3'):
         randRec.sort(key=compRecipe, reverse=True)
         for i, rec in enumerate(randRec[:min(3, len(randRec))]):
-            print(str(i+1) + ": " + rec._name + ", rating: " + str(rec._rating))
-
-    begin()
+            print(str(i+1) + ". ",end="")
+            rec.printMini()
+        print()
+        print("Would you like to see a full recipe?")
+        whichRec = input("Enter 1, 2, 3, or no: ")
+        print()
+        while whichRec.isnumeric():
+            if whichRec == '1' and len(randRec) > 0:
+                print(randRec[0])
+            elif whichRec == '2' and len(randRec) > 1:
+                print(randRec[1])
+            elif whichRec == '3' and len(randRec) > 2:
+                print(randRec[2])
+            whichRec = input("Show another? Enter 1, 2, 3, or no: ")
+            print()
 
 def showRecipe():
     recipeName = input("Enter a recipe name: ").strip()
@@ -107,10 +132,11 @@ def showRecipe():
 
     print(rec)
 
-    begin()
-
 def enterRecipe():
     inputName = input("Enter recipe name: ")
+    print("Intensity options: " + ", ".join(INTENSITIES))
+    inputIntensity = input("Enter an intensity: ")
+    inputIntensity = process.extractOne(inputIntensity, INTENSITIES)[0]
     inputRating = None
     while not inputRating:
         try:
@@ -128,7 +154,8 @@ def enterRecipe():
     for tag in inputTags:
         if tag.lower() not in allIngredients:
             allIngredients.add(tag)
-    newRecipe = recipe(inputName, str(inputRating), link=inputLink, recipeText=inputText, ingredients=[",".join(inputTags)])
+    newRecipe = recipe(inputName, intensity=inputIntensity, rating=str(inputRating), link=inputLink, recipeText=inputText, ingredients=[",".join(inputTags)])
+    print()
     print("Adding " + str(newRecipe))
     print("Is this ok? Enter \'no' to cancel.")
     if input().lower() == 'no':
@@ -138,27 +165,29 @@ def enterRecipe():
         file.write(inputName + "," + str(inputRating) + "," + ",".join(inputTags) + "\n")
     recipes.append(newRecipe)
 
-    begin()
-
 def begin():
-    print()
-    print("1: Suggest some recipes.")
-    print("2: Show me a specific recipe.")
-    print("3: Add a recipe.")
-    print("4: Exit program.")
-    response = input("Enter a number: ")
-    print()
-    if response == '1':
-        suggestRecipe()
-    elif response == '2':
-        showRecipe()
-    elif response == '3':
-        enterRecipe()
-    elif response == '4':
-        return
-    else:
-        print("1, 2, or 3, please!")
-        begin()
+    while True:
+        print()
+        print("1: Suggest some recipes.")
+        print("2: Show me a specific recipe.")
+        print("3: Add a recipe.")
+        print("4: Exit program.")
+        response = input("Enter a number: ")
+        print()
+        while True:
+            if response == '1':
+                suggestRecipe()
+                break
+            elif response == '2':
+                showRecipe()
+                break
+            elif response == '3':
+                enterRecipe()
+                break
+            elif response == '4':
+                return
+            else:
+                response = input("1, 2, or 3, please! ")
 
 if __name__ == "__main__":
     makeLists()
